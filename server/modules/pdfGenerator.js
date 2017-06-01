d769afd40d2b3ffc4e867ca3ddf39291dab00a21
guywithcookies <Benni.Schimke@web.de>
2017-06-01 14:14:58 +0200

    added pdfGenerator(working version)

--- server/modules/pdfGenerator.js
+++ server/modules/pdfGenerator.js
@@ -32,124 +32,34 @@ var pdfGenerator = {
          -relevant eventData
          -time range (date, range(y, m, w)
          */
-        //test
-        var docData = {
-            forename: "Benjamin",
-            username: "Schimke",
-            timeRange: {
-                range: "y",
-                date: moment()
-            },
-            events: [
-                { _id: "5906daf2618d2915748d80e6",
-                title: 'Blabla',
-                startsAt: 1493524800,
-                endsAt: 1493542800,
-                username: 'Schimke',
-                comment: 'qwewqewqeq',
-                object: 'Blabla',
-                activity: 'qewqewq',
-                type: 'morning',
-                __v: 0 },
-                { _id: "5906daf2618d2915748d80e7",
-                    title: 'undefined',
-                    startsAt: 1493542800,
-                    endsAt: 1493550000,
-                    username: 'Schimke',
-                    comment: 'undefined',
-                    object: 'undefined',
-                    activity: 'undefined',
-                    type: 'break',
-                    __v: 0 },
-                { _id: "5906daf2618d2915748d80e8",
-                    title: 'Blabla',
-                    startsAt: 1493550000,
-                    endsAt: 1493564400,
-                    username: 'Schimke',
-                    comment: 'qwewqewqeq',
-                    object: 'Blabla',
-                    activity: 'qewqewq',
-                    type: 'afternoon',
-                    __v: 0 },
-                { _id: "590a27cb224e9c5e9f224b5f",
-                    title: 'asdf',
-                    startsAt: 1493784000,
-                    endsAt: 1493798400,
-                    username: 'Schimke',
-                    comment: 'asdf',
-                    object: 'asdf',
-                    activity: 'asdf',
-                    type: 'morning',
-                    __v: 0 },
-                { _id: "590a27cb224e9c5e9f224b60",
-                    title: 'undefined',
-                    startsAt: 1493798400,
-                    endsAt: 1493809200,
-                    username: 'Schimke',
-                    comment: 'undefined',
-                    object: 'undefined',
-                    activity: 'undefined',
-                    type: 'break',
-                    __v: 0 },
-                { _id: "590a27cb224e9c5e9f224b61",
-                    title: 'asdf',
-                    startsAt: 1493809200,
-                    endsAt: 1493820000,
-                    username: 'Schimke',
-                    comment: 'asdf',
-                    object: 'asdf',
-                    activity: 'asdf',
-                    type: 'afternoon',
-                    __v: 0 },
-                { _id: "590b56e7224e9c5e9f224b62",
-                    title: 'Test',
-                    startsAt: 1493870400,
-                    endsAt: 1493892000,
-                    username: 'Schimke',
-                    comment: 'Nocsh ein Test',
-                    object: 'Test',
-                    activity: 'Test',
-                    type: 'morning',
-                    __v: 0 },
-                { _id: "590b56e7224e9c5e9f224b63",
-                    title: 'Pause',
-                    startsAt: 1493892000,
-                    endsAt: 1493895600,
-                    username: 'Schimke',
-                    comment: 'undefined',
-                    object: 'undefined',
-                    activity: 'undefined',
-                    type: 'break',
-                    __v: 0 },
-                { _id: "590b56e7224e9c5e9f224b64",
-                    title: 'Test',
-                    startsAt: 1493895600,
-                    endsAt: 1493906400,
-                    username: 'Schimke',
-                    comment: 'Noch ein Test',
-                    object: 'Test',
-                    activity: 'Testd',
-                    type: 'afternoon',
-                    __v: 0 } ]
-        };
         var count = {};
         var content = [];
+        //cover sheet
+        content.push({
+            image:"../admin/public/img/logo.png",
+            style:["logo","center"]
+        });
 
-        //Header
-        content.push({text: 'Arbeitszeitnachweis für ' + docData.forename + " " + docData.username, style: 'header'});
+        content.push({text: 'Arbeitszeitnachweis', style: ['center', 'header']});
+
+        content.push({text: 'für ' + docData.forename + " " +
+        docData.username, style: ['center', 'subheader']});
+
         //Timedata
         var time = docData.timeRange;
         switch (time.range) {
             case "y":
             case "year":
-                content.push({text: 'Jahr: ' + moment(time.date).year()});
+                content.push({text: moment(time.date).year(),  style: ['center', 'subheader']});
                 count.startWeek = 0;
                 count.weeks = moment(time.date).weeksInYear();
                 break;
             case "m":
             case "month":
-                content.push({text: 'Monat: ' + moment(time.date).format('MMMM')});
-                content.push({text: 'Jahr: ' + moment(time.date).year()});
+                content.push({
+                    text: moment(time.date).format('MMMM')+" "+moment(time.date).year(),
+                    style: ['center', 'subheader']
+                });
                 count.startWeek = moment(time.date).startOf('month').week();
                 //Add one to get the last days
                 count.weeks = moment(time.date).add(1, "month").startOf('month').week() - count.startWeek + 1;
@@ -163,54 +73,74 @@ var pdfGenerator = {
                 content.push({
                     text: 'Woche ' + weekNumber +
                     ' vom ' + beginningOfWeek.format("DD") + " " + beginningOfWeek.format("MMMM") +
-                    ' bis ' + endOfWeek.format("DD") + " " + endOfWeek.format("MMMM") + Array(120).join(" ") +
-                    'Jahr: ' + moment(time.date).year()
+                    ' bis ' + endOfWeek.format("DD") + " " + endOfWeek.format("MMMM"),
+                    style: ['center', 'subheader']
+                });
+
+                content.push({
+                    text: 'Jahr: ' + moment(time.date).year(),
+                    style: ['center', 'subheader']
                 });
 
                 count.startWeek = weekNumber;
                 count.weeks = 1;
                 break;
         }
-        console.log(count);
-        //TimeTable
 
+        content.push({text:'', pageBreak:"after", pageOrientation:"landscape"});
+
+
+        //TimeTable
         //First sort the events by week
         var sortedEvents = {};
 
         for (var event in docData.events){
             if (docData.events.hasOwnProperty(event)) {
-                console.log(moment(docData.events[event].startsAt*1000).toISOString());
-                var number = moment(docData.events[event].startsAt*1000).week();
-                if(!sortedEvents.hasOwnProperty(number)){
-                    sortedEvents[number] = []
+                var week = moment(docData.events[event].startsAt*1000).week();
+                if(!sortedEvents.hasOwnProperty(week)){
+                    sortedEvents[week] = {};
                 }
-                sortedEvents[number].push(docData.events[event]);
+                var weekday = moment(docData.events[event].startsAt*1000).format("dddd");
+                if(!sortedEvents[week].hasOwnProperty(weekday)){
+                    sortedEvents[week][weekday] = [];
+                }
+                sortedEvents[week][weekday].push(docData.events[event]);
             }
         }
 
         //now sort by date
         for (var week in sortedEvents) {
             if (sortedEvents.hasOwnProperty(week)) {
-                sortedEvents[week]= sortedEvents[week].sort(function (a, b) {
-                    return a.startsAt - b.startsAt;
-                })
+                for (var day in sortedEvents[week]) {
+                    if (sortedEvents[week].hasOwnProperty(day)) {
+                        sortedEvents[week][day] = sortedEvents[week][day].sort(function (a, b) {
+                            return a.startsAt - b.startsAt;
+                        })
+                    }
+                }
             }
         }
-        //now sort by weekday
-        for (var day in sortedEvents[week]) {
-            if (sortedEvents[week].hasOwnProperty(week)) {
-                sortedEvents[week]= sortedEvents[week].sort(function (a, b) {
-                    return a.startsAt - b.startsAt;
-                })
-            }
-        }
-        console.log(sortedEvents);
-        //Generate 1 Table for every Week
-        for(var week=count.startWeek; week<count.weeks+count.startWeek; week++) {
 
+        //Generate 1 Table for every Week
+        for(week=count.startWeek; week<count.weeks+count.startWeek; week++) {
             if(!sortedEvents.hasOwnProperty(week)){
                 continue;
             }
+
+            //add some description when month or year so we can identify the week
+            if(time.range === "y" || time.range === "year" ||
+                time.range === "m" || time.range === "month"){
+                var beginningOfWeek = moment().week(week).startOf('week');
+                var endOfWeek = moment().week(week).startOf('week').add(6, 'days');
+                content.push({
+                    text: 'Woche ' + week +
+                    ' vom ' + beginningOfWeek.format("DD") + " " + beginningOfWeek.format("MMMM") +
+                    ' bis ' + endOfWeek.format("DD") + " " + endOfWeek.format("MMMM"),
+                    style: "subheader",
+                    margin:[0, 0, 0, 25]
+                });
+            }
+
             var body = [
                 //HeaderColumns
                 [
@@ -218,24 +148,65 @@ var pdfGenerator = {
                     {text: 'Objekt', style: 'tableHeader', alignment: 'center'},
                     {text: 'Tätigkeit', style: 'tableHeader', alignment: 'center'},
                     {text: 'Bemerkungen', style: 'tableHeader', alignment: 'center'},
-                    {text: 'Arbeitsbeginn', style: 'tableHeader', alignment: 'center'},
-                    {text: 'Arbeitsende', style: 'tableHeader', alignment: 'center'},
+                    {text: 'Beginn', style: 'tableHeader', alignment: 'center'},
+                    {text: 'Ende', style: 'tableHeader', alignment: 'center'},
                     {text: 'Ist-AZ', style: 'tableHeader', alignment: 'center'},
                     {text: 'Pause', style: 'tableHeader', alignment: 'center'},
                     {text: 'GesamtStd', style: 'tableHeader', alignment: 'center'}]
             ];
 
-            for(event in sortedEvents){
-                if(sortedEvents.hasOwnProperty(event)){
-                    var ev = sortedEvents[event];
-                    body.push([
-                        moment(ev.startsAt).weekday(),
-                        ev.object,
-                        ev.activity,
-                        ev.comment,
-                        moment(ev.startsAt).format("hh:mm"),
-                        moment(ev.endsAt).format("hh:mm"),
+            for(var day in sortedEvents[week]){
+                if(sortedEvents[week].hasOwnProperty(day)){
+                    var day = sortedEvents[week][day];
+                    var firstDay = true,
+                        pause = {
+                            hours:0,
+                            minutes:0
+                        };
+                    var workSum = {
+                        hours:0,
+                        minutes:0
+                    };
 
+                    for(var event in day){
+                        if(day.hasOwnProperty(event)){
+                            var ev = day[event];
+                            var diff =  moment((ev.endsAt-ev.startsAt)*1000).subtract(1, "hour");
+                            weekday = firstDay ? {text:moment(ev.startsAt*1000).format("dddd"), rowSpan: Object.keys(day).length}:'';
+                            firstDay = false;
+                            if(ev.type === "break"){
+                                pause["hours"]   += diff.hours();
+                                pause["minutes"] += diff.minutes();
+                                continue;
+                            }else{
+                                workSum["hours"]   += diff.hours();
+                                workSum["minutes"] += diff.minutes();
+                            }
+
+                            body.push([
+                                weekday,
+                                ev.object,
+                                ev.activity,
+                                ev.comment,
+                                {text:moment(ev.startsAt*1000).format("H:mm"), style:"center"},
+                                {text:moment(ev.endsAt*1000).format("H:mm"), style:"center"},
+                                {text:diff.format("H:mm"), style:"center"},
+                                '',
+                                ''
+                            ])
+                        }
+                    }
+                    //push the sum of pause and workinghours
+                    body.push([
+                        '',
+                        '',
+                        '',
+                        '',
+                        '',
+                        '',
+                        '',
+                        {text:moment(pause).format("H:mm"), style:"center"},
+                        {text:moment(workSum).format("H:mm"), style:"center"}
                     ])
                 }
             }
@@ -244,42 +215,36 @@ var pdfGenerator = {
                 style: 'table',
                 color: '#444',
                 table: {
-                    widths: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto'],
+                    widths: ["10%", "15%", '20%', '20%', '7%', '7%', '7%', '7%', '7%'],
                     headerRows: 1,
-                    body: body
-                }
+                    body: body,
+                },
+                pageBreak:"after",
+
             });
         }
-        console.log(content);
         return content;
     },
 
     defaultDefinition: {
-        content: [
-            {text: 'Arbeitszeitnachweis', style: 'header'},
-            'Official documentation is in progress, this document is just a glimpse of what is possible with pdfmake and its layout engine.',
-            {text: 'A simple table (no headers, no width specified, no spans, no styling)', style: 'subheader'},
-            'The following table has nothing more than a body array',
-            {
-                style: 'tableExample',
-                table: {
-                    body: [
-                        ['Column 1', 'Column 2', 'Column 3'],
-                        ['One value goes here', 'Another one here', 'OK?']
-                    ]
-                }
-            }
-        ],
-        pageOrientation: 'landscape',
+        pageOrientation: 'portrait',
         styles: {
             header: {
                 fontSize: 18,
                 bold: true,
+                underlined: true,
                 margin: [0, 0, 0, 10]
             },
-            subheader: {
-                fontSize: 16,
+            subsubheader:{
+                fontSize: 15,
                 bold: true,
+                italics: false,
+                margin: [0, 10, 0, 0]
+            },
+            subheader: {
+                fontSize: 13,
+                bold: false,
+                italics: true,
                 margin: [0, 10, 0, 5]
             },
             table: {
@@ -289,8 +254,14 @@ var pdfGenerator = {
                 bold: true,
                 fontSize: 13,
                 color: 'black'
+            },
+            logo:{
+                margin: [0, 50, 15, 25]
+            },
+            center:{
+                alignment: 'center'
             }
-        },
+        }
     }
 };
 
